532aff4b4b1f92b2d729ee2d5682fba5d58cba4d
MarkGuo2002 <100450691@alumnos.uc3m.es>
2024-04-11 20:14:09 +0200

    Just experimenting

---
+++ TemperatureWarriorCode/personal/yikai/CodeYikai.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemperatureWarriorCode.personal.yikai
+{
+    internal class CodeYikai
+        string hello = "python is better";
+    {
+    }
+}
