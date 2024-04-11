3673eefd72c7ee8b554cf1fcc99894fc67cf121d
DARIO CABALLERO POLO <100451112@alumnos.uc3m.es>
2024-04-11 02:59:18 +0200

    Initial changes + Fan interface

--- TemperatureWarriorCode/MeadowApp.cs
+++ TemperatureWarriorCode/MeadowApp.cs
@@ -17,13 +17,51 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using TemperatureWarriorCode.Web;
 using NETDuinoWar;
+using Meadow.Gateways.Bluetooth;
 
 
-namespace TemperatureWarriorCode {
-    public class MeadowApp : App<F7FeatherV2> {
+namespace TemperatureWarriorCode
+{
+    public class Fan
+    {
+        // Fan pin
+        public IDigitalOutputPort pin;
+        public IPwmPort pwm;
+
+        // Constructor
+        public Fan(F7FeatherV2 device, IPin pin)
+        {
+            this.pwm = device.CreatePwmPort(pin, new Frequency(25000, Frequency.UnitType.Hertz), 0f);
+            pwm.Start();
+            Console.WriteLine("Fan initialized");
+        }
+
+        // Methods
+        public void TurnOn()
+        {
+            pwm.DutyCycle = 0.1f;
+        }
+
+        public void TurnOff()
+        {
+            pwm.DutyCycle = 0f;
+        }
+        // NOTE: a duty cycle of 0 appears to output the maximum voltage to the pin (4.4V)
+        // A duty cycle of 1 appears to output 3.3V to the pin
+        // A duty cycle of 0.1 appears to output 0.35V to the pin
+    }
+
+
+    public class MeadowApp : App<F7FeatherV2>
+    {
 
         //Temperature Sensor
-        AnalogTemperature sensor;
+        AnalogTemperature sensor; // outdated
+
+        //Display
+        St7789 display; //not used
+        MicroGraphics graphics;
+
 
         //Time Controller Values
         public static int total_time = 0;
@@ -32,67 +70,78 @@ namespace TemperatureWarriorCode {
 
         public int count = 0;
 
-        // Entry point of the application
-        public override async Task Run() {
-            if (count == 0) {
+        //Fan
+        static Fan fan;
+
+
+        public override async Task Run()
+        {
+            if (count == 0)
+            {
                 Console.WriteLine("Initialization...");
 
-                // TODO uncomment when needed 
+                // TODO uncomment when needed
+
+                // Create the fan passing the Device and the pin
+                fan = new Fan(Device, Device.Pins.D02);
+
                 // Temperature Sensor Configuration
                 sensor = new AnalogTemperature(analogPin: Device.Pins.A01, sensorType: AnalogTemperature.KnownSensorType.TMP36);
-                // sensor.Temperature.Value.Celsius // To do it this way we would need to create a new thread to update the temperature (we are not doing it this way)
-                sensor.TemperatureUpdated += AnalogTemperatureUpdated;  // This way we are updating the temperature in the main thread by using a callback
-                sensor.StartUpdating(TimeSpan.FromSeconds(2));  // We are updating the temperature every 2 seconds (Change this value if needed)
-                // Note: DHT22 sensor only updates every 2 seconds, so there is no need to update it more frequently if it is used
+                sensor.TemperatureUpdated += AnalogTemperatureUpdated;
+                sensor.StartUpdating(TimeSpan.FromSeconds(2));
 
-                // TODO Display Configuration (uncomment when needed) (WE DO NOT NEED THE DISPLAY)
-                //var config = new SpiClockConfiguration(new Frequency(48000, Frequency.UnitType.Kilohertz), SpiClockConfiguration.Mode.Mode3);
-                //var spiBus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.COPI, Device.Pins.CIPO, config);
-                //display = new St7789(
-                //spiBus: spiBus,
-                //chipSelectPin: null,
-                //dcPin: Device.Pins.D01,
-                //resetPin: Device.Pins.D00,
-                //width: 240, height: 240);
-                //graphics = new MicroGraphics(display);
-                //graphics.Rotation = RotationType._270Degrees;
 
                 // TODO Local Network configuration (uncomment when needed)
-                //var wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
-                //wifi.NetworkConnected += WiFiAdapter_ConnectionCompleted;
+                var wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
+                wifi.NetworkConnected += WiFiAdapter_ConnectionCompleted;
 
-                ////WiFi Channel
-                //WifiNetwork wifiNetwork = ScanForAccessPoints(Secrets.WIFI_NAME);
+                Console.WriteLine($"ssid: {Secrets.WIFI_NAME}\npswd: {Secrets.WIFI_PASSWORD}");
+                //WiFi Channel
+                WifiNetwork wifiNetwork = ScanForAccessPoints(Secrets.WIFI_NAME);
+                Console.WriteLine($"wifinetwork{wifiNetwork}");
 
-                //wifi.NetworkConnected += WiFiAdapter_WiFiConnected;
-                //await wifi.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD);
+                wifi.NetworkConnected += WiFiAdapter_WiFiConnected;
+                await wifi.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD);
 
-                //string IPAddress = wifi.IpAddress.ToString();
+                string IPAddress = wifi.IpAddress.ToString();
 
-                // Connnect to the WiFi network.
-                /*
+                //Connnect to the WiFi network.
                 Console.WriteLine($"IP Address: {IPAddress}");
                 Data.IP = IPAddress;
-                if (!string.IsNullOrWhiteSpace(IPAddress)) {
+                if (!string.IsNullOrWhiteSpace(IPAddress))
+                {
                     Data.IP = IPAddress;
                     WebServer webServer = new WebServer(wifi.IpAddress, Data.Port);
-                    if (webServer != null) {
-                        webServer.Start();  // After the start, it will be running in the background
+                    if (webServer != null)
+                    {
+                        webServer.Start();
                     }
                 }
-                */
 
                 // TODO Display initialization (uncomment when needed)
                 //Display();
 
-                Console.WriteLine("Meadow Initialized!");
-
-                count = count + 1;
+                count++;
             }
+
+            ////////////////////// TESTING FAN //////////////////////
+            while (true)
+            {
+                // Turn on the fan
+                Console.WriteLine("Turning on the fan");
+                fan.TurnOn();
+                Thread.Sleep(5000);
+                // Turn off the fan
+                Console.WriteLine("Turning off the fan");
+                fan.TurnOff();
+                Thread.Sleep(5000);
+            }
+            /////////////////// END OF TESTING FAN //////////////////
         }
 
         //TW Combat Round
-        public static void StartRound() {
+        public static void StartRound()
+        {
 
             Stopwatch timer = Stopwatch.StartNew();
             timer.Start();
@@ -113,12 +162,13 @@ namespace TemperatureWarriorCode {
             Data.is_working = true;
 
             //define ranges
-            for (int i = 0; i < Data.temp_min.Length; i++) {
+            for (int i = 0; i < Data.temp_min.Length; i++)
+            {
                 Console.WriteLine(Data.temp_max[i]);
                 temperatureRanges[i] = new TemperatureRange(double.Parse(Data.temp_min[i]), double.Parse(Data.temp_max[i]), int.Parse(Data.round_time[i]) * 1000);
                 total_time += int.Parse(Data.round_time[i]);
             }
-            
+
             //Initialization of timecontroller with the ranges
             timeController.DEBUG_MODE = false;
             success = timeController.Configure(temperatureRanges, total_time * 1000, Data.refresh, out error_message);
@@ -134,14 +184,13 @@ namespace TemperatureWarriorCode {
 
             Console.WriteLine("STARTING");
 
-            //THE TW START WORKING
-            while (Data.is_working) {
-                // PUT HERE THE CODE TO CONTROL THE HEATER AND COOLER
+            //THE TW (Temperature Warrior) START WORKING
+            while (Data.is_working)
+            {
+                // Turn on the fan
+                fan.TurnOn();
 
                 //This is the time refresh we did not do before
-                // We are sleeping the time we need to sleep to make the refresh time be the same as the one we want
-                // We need to update sleep_time to adjust it to the time that our code takes to run, so that timeController.RegisterTemperature
-                // is called at EXACTLY thetime we are told to do so
                 Thread.Sleep(Data.refresh - sleep_time);
 
                 //Temperature registration
@@ -150,6 +199,9 @@ namespace TemperatureWarriorCode {
                 regTempTimer.Restart();
 
             }
+            // END OF THE ROUND
+            // Turn off the fan
+            fan.TurnOff();
             Console.WriteLine("Round Finish");
             t.Abort();
 
@@ -162,12 +214,15 @@ namespace TemperatureWarriorCode {
         }
 
         //Round Timer
-        private static void Timer() {
+        private static void Timer()
+        {
             Data.is_working = true;
-            for (int i = 0; i < Data.round_time.Length; i++) {
+            for (int i = 0; i < Data.round_time.Length; i++)
+            {
                 Data.time_left = int.Parse(Data.round_time[i]);
 
-                while (Data.time_left > 0) {
+                while (Data.time_left > 0)
+                {
                     Data.time_left--;
                     Thread.Sleep(1000);
                 }
@@ -175,27 +230,31 @@ namespace TemperatureWarriorCode {
             Data.is_working = false;
         }
 
-        //Display Theme
-
-
         //Temperature and Display Updated
-        void AnalogTemperatureUpdated(object sender, IChangeResult<Meadow.Units.Temperature> e) {
-            // We can filter the high frequency changes on the temperature (DO THIS SPECIALLY FOR THE BAD SENSOR)
+        void AnalogTemperatureUpdated(object sender, IChangeResult<Meadow.Units.Temperature> e)
+        {
+
+
             Data.temp_act = Math.Round((Double)e.New.Celsius, 2).ToString();
+
             Console.WriteLine($"Temperature={Data.temp_act}");
         }
 
-        void WiFiAdapter_WiFiConnected(object sender, EventArgs e) {
-            if (sender != null) {
+        void WiFiAdapter_WiFiConnected(object sender, EventArgs e)
+        {
+            if (sender != null)
+            {
                 Console.WriteLine($"Connecting to WiFi Network {Secrets.WIFI_NAME}");
             }
         }
 
-        void WiFiAdapter_ConnectionCompleted(object sender, EventArgs e) {
+        void WiFiAdapter_ConnectionCompleted(object sender, EventArgs e)
+        {
             Console.WriteLine("Connection request completed.");
         }
 
-        protected WifiNetwork ScanForAccessPoints(string SSID) {
+        protected WifiNetwork ScanForAccessPoints(string SSID)
+        {
             WifiNetwork wifiNetwork = null;
             ObservableCollection<WifiNetwork> networks = new ObservableCollection<WifiNetwork>(Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>().Scan()?.Result?.ToList()); //REVISAR SI ESTO ESTA BIEN
             wifiNetwork = networks?.FirstOrDefault(x => string.Compare(x.Ssid, SSID, true) == 0);
